748e3a846026ed6cf0b6a960fa04603d94825b4a
andrehirano10 <110587452+andrehirano10@users.noreply.github.com>
2022-12-20 18:13:47 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const quotes = [
     { quote: "Anybody who preserve the ability to recognize beauty\nwill never get old", author: "Franz Kafka" },
     { quote: "Only through suffering can we find ourselves", author: "Fyodor Dostoevsky" },
     { quote: "Don't be afraid to give up the good to go for the best.", author: "John D. Rockefeller" },
-    { quote: "It is not true that people stop pursuing dreams \nbecause they grow old they grow old because they stop pursuing dreams.", author: "Gabriel Garcia Marzuez" },
+    { quote: "It is not true that people stop pursuing dreams\nbecause they grow old they grow old because they stop pursuing dreams.", author: "Gabriel Garcia Marzuez" },
     { quote: "Music in the soul can be heard by the universe.", author: "Lao Tzu"},
     { quote: "Do not fear failure but rather fear not trying.", author: "Roy T. Bennet"},
     { quote: "God is a comedian playing to an audience that is too\nafraid to laugh.", author: "Voltaire"},
@@ -12,7 +12,7 @@ const quotes = [
     { quote: "Don’t you wonder sometimes, what might have\nhappened if you tried?", author: "Kazuo Ishiguro"},
     { quote: "The purpose of life is not\nto be happy. It is to be useful, to be honorable, to be compassionate, \nto have it make some difference that you have lived and lived well.", author: "Ralph Waldo Emerson"},
     { quote: "My soul is a hidden orchestra; I know not what\ninstruments, what fiddlestrings and harps,\ndrums and tamboura I sound and clash inside myself.\nAll I hear is the symphony.", author: "Fernando Pessoa"},
-    { quote: "The purpose of life is not to be happy. It is to be useful, \nto be honorable, \nto be compassionate, \nto have it make some difference that you have lived and lived well.", author: "Miyamoto Musashi"},
+    { quote: "The purpose of life is not to be happy. It is to be useful,\nto be honorable, \nto be compassionate, \nto have it make some difference that you have lived and lived well.", author: "Miyamoto Musashi"},
     { quote: "If a thing is worth doing, it is worth doing well. If it is worth having, it is worth waiting for.\nIf it is worth attaining, it is worth fighting for. If it is worth experiencing, it is worth putting aside time for.", author: "Oscar Wilde"}
 
   ];
